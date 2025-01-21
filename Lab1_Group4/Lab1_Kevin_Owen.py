0887cad36522da0eab5c06d3484a8a0d6b742b3d
Kevin <kevin41212@gmail.com>
2025-01-21 15:49:02 -0500

    comment header
    created comment header

---
+++ Lab1_Group4/Lab1_Kevin_Owen.py
@@ -0,0 +1,5 @@
+# Name: Kevin, Owen
+# Date 2025-01-21
+# Last Modified: 2025-01-
+# Description: A program that generates secure passwords based on user
+# criteria such as length, number of letters, digits, and special characters
