30043bdfba9037970b7a0532a3a47837d70846d0
Kevin <kevin41212@gmail.com>
2025-01-21 16:43:06 -0500

    added error checking to the code and stuff

--- Lab1_Group4/Lab1_Kevin_Owen.py
+++ Lab1_Group4/Lab1_Kevin_Owen.py
@@ -8,17 +8,30 @@ import random
 MAX_LENGTH = 28
 MIN_LENGTH = 4
 is_valid = True
+error_message_valid = "Please enter a valid integer"
 while is_valid == True:
         passwordLength = input("Please enter the desired password length between 4-28: ")
         if passwordLength.isnumeric():
             passwordLength = int(passwordLength)
             if passwordLength < 4 or passwordLength > 28:
-                print("error")
+                print("Password is not in range: Please enter a integer between 4 and 28")
             elif passwordLength > 4 or passwordLength < 28:
-                passwordChars = int(input("Numbers"))
-                passwordNumbers = int(input("Numbers"))
-                for count in range(passwordLength):
-                    number = random.randint(0,9)
-                    print(number, end="")
+                while is_valid == True:
+                    passwordChars = input("Please enter the number of characters for the password: ")
+                    if passwordChars.isnumeric():
+                        passwordChars = int(passwordChars)
+                        while is_valid == True:
+                            passwordNumbers = input("Please enter the number of numbers for the password: ")
+                            if passwordNumbers.isnumeric():
+                                passwordNumbers = int(passwordNumbers)
+                                in_range = passwordChars + passwordNumbers
+                                if in_range > passwordLength:
+                                    print("To many Alphanumeric characters. Please enter less Numbers")
+                                else:
+                                    print('yay')
+                            else:
+                                print(error_message_valid)
+                    else:
+                        print(error_message_valid)
         else:
-            print("Please enter a valid integer")
+            print(error_message_valid)
