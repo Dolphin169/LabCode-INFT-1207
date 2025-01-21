c6b66741e4fbf535ea96446ce7ea2a3e791046e4
Kevin <kevin41212@gmail.com>
2025-01-21 16:27:34 -0500

    Kevin-Started base code

--- Lab1_Group4/Lab1_Kevin_Owen.py
+++ Lab1_Group4/Lab1_Kevin_Owen.py
@@ -5,10 +5,20 @@
 # criteria such as length, number of letters, digits, and special characters
 
 import random
-
-passwordLength = int(input("Enter the desired password length: "))
-for count in range(passwordLength):
-    number = random.randint(0,9)
-    print(number, end="")
-
-
+MAX_LENGTH = 28
+MIN_LENGTH = 4
+is_valid = True
+while is_valid == True:
+        passwordLength = input("Please enter the desired password length between 4-28: ")
+        if passwordLength.isnumeric():
+            passwordLength = int(passwordLength)
+            if passwordLength < 4 or passwordLength > 28:
+                print("error")
+            elif passwordLength > 4 or passwordLength < 28:
+                passwordChars = int(input("Numbers"))
+                passwordNumbers = int(input("Numbers"))
+                for count in range(passwordLength):
+                    number = random.randint(0,9)
+                    print(number, end="")
+        else:
+            print("Please enter a valid integer")
