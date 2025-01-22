04175c3b9a7b7d9fbb4a60c764b4391034ba7201
Kevin <kevin41212@gmail.com>
2025-01-22 13:08:35 -0500

    got the password length working

--- Lab1_Group4/Lab1_Kevin_Owen.py
+++ Lab1_Group4/Lab1_Kevin_Owen.py
@@ -19,15 +19,12 @@ def random_special():
 # Function to get user input (skeleton)
 def get_user_input(prompt, min_value, max_value):
     # Implement logic to get valid user input within a range
-    if prompt < min_value or prompt > max_value:
-        # Print error message
-        print(f"Password is not in range: Please enter a integer between {min_value} and {max_value}")
-        is_valid = True
-        # Check the password length is between 4 and 28
-    elif prompt > min_value and prompt < max_value:
-        pass
-
-    pass
+        if prompt < min_value or prompt > max_value:
+            # Print error message
+            print(f"Password is not in range: Please enter a integer between {min_value} and {max_value}")
+        else:
+            pass
+            return True
 
 # Function to generate a password (skeleton)
 def generate_password(length, num_letters, num_digits, num_specials):
@@ -44,13 +41,19 @@ def main():
     # Step 1: Get user inputs for password length and character distribution
     is_valid = True
     while is_valid == True:
-        try:
-            passwordLength = int(input("Please enter the desired password length between 4-28: "))
-            get_user_input(passwordLength, MIN_VALUE, MAX_VALUE)
-            is_valid = False
-        except:
-            print("error")
-            is_valid = True    # Step 2: Validate user inputs
+            password_length = input("Please enter the desired password length between 4-28: ")
+            if password_length.isnumeric():
+                password_length = int(password_length)
+                if get_user_input(password_length, MIN_VALUE, MAX_VALUE):
+                    is_valid = False
+            else:
+                print("Please enter a valid integer")
+
+
+    password_letters = int(input("Please enter the amount of letters"))
+    password_numbers = int(input("Please enter the amount of numbers"))
+    password_special = int(input("Please enter the amount of special characters"))
+    # Step 2: Validate user inputs
 
     # Step 3: Generate the password
 
