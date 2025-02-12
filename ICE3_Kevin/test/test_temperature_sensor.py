eb1c335bfcbfc38e64914a6441c29483578a0907
Kevin <kevin41212@gmail.com>
2025-02-12 16:34:08 -0500

    started ICE 3 a little
    I did that

---
+++ ICE3_Kevin/test/test_temperature_sensor.py
@@ -0,0 +1,7 @@
+import unittest
+from ICE3_Kevin.src.temperature_sensor import process_temperatures
+
+class TestTemperatureSensor(unittest.TestCase):
+
+    def test_process_temerpatures(self):
+        print("")
